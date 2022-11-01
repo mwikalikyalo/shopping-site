98b54b417be4d569a8350fe435723a2af153bc0d
mwikalikyalo <winniemwikali07@gmail.com>
2022-11-01 10:21:30 +0300

    Add font awesome to the project and add the login structure and styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faSquareTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  faEnvelope = faEnvelope;
+  faFacebook = faFacebook;
+  faSquareTwitter = faSquareTwitter;
+  faCartShopping = faCartShopping;
 
   constructor() { }
 
