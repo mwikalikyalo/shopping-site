b6fb77742d3829813119ef3468ab681c16f9724e
mwikalikyalo <winniemwikali07@gmail.com>
2022-11-01 10:59:31 +0300

    Add the routes paths for the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisplayComponent } from './display/display.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '/signup', component: SignupComponent },
+  { path: '/login', component: LoginComponent },
+  { path: '/footer', component: FooterComponent },
+  { path: '/display', component: DisplayComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
