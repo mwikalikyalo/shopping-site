2d9b2448b3893da160569704b509eeedfab20e50
mwikalikyalo <winniemwikali07@gmail.com>
2022-11-03 09:59:40 +0300

    Add the create product method, import HttpClient and HttpHeaders to the service

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root' 
 })
 export class ItemsService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  //from database
+  createProduct(products: {pName: string, desc: string, price: string}){
+    const headers= new HttpHeaders({"myHeader": "procademy"})
+    this.http.post<{name: string}>(
+    'https://angularbyprocademy-default-rtdb.firebaseio.com/products.json',
+    products, {headers: headers})
+    .subscribe((res) => {
+    console.log(res);
+   });
+  }
+
+  fetchProduct(){
+
+  }
+
+  deleteProduct(){
+
+  }
+
+  deleteAllProducts(){
+
+  }
+
+
+  
 }
