7b74d71e6990b6d91d2c25ecdfff64e0997b956a
mwikalikyalo <winniemwikali07@gmail.com>
2022-11-07 10:36:50 +0300

    Inject the auth service to the guard service to verify state of the user

--- src/app/guard.service.ts
+++ src/app/guard.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router'
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardService implements CanActivate {
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-      return false;
+
+  constructor(private authService: AuthService) {
+
   }
-  constructor() { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+      if(this.authService.isAuthenticated()){
+          return true;
+      }else {
+        return false;
+      }
+  }
+  
 }
